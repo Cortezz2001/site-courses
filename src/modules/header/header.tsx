a1942fa63b86c8fff8eb5b46b2974a8a7c7c2029
Maxim Fedkevich <cortezzcloud@gmail.com>
2024-04-01 14:50:51 +0500

    menu content maxwidth 1330px

--- src/modules/header/header.tsx
+++ src/modules/header/header.tsx
@@ -14,82 +14,80 @@ import {
 const Header = () => {
     return (
         <Container name="header" fluid>
-            <Menu
-                name="mainheader"
-                borderless
-                attached="top"
-                inverted
-                //style={{ backgroundColor: "black" }}
-            >
-                <MenuItem position="left" name="logo">
-                    <Link href="/">
-                        <Image src="/logo.svg" size="tiny" />
-                    </Link>
-                </MenuItem>
-                <MenuItem position="right">+77711739922</MenuItem>
-                <MenuItem>
-                    <Button basic inverted>
-                        EN
-                    </Button>
-                    <Button basic inverted style={{ marginLeft: "10px" }}>
-                        USD
-                    </Button>
-                    <Button basic inverted style={{ marginLeft: "20px" }}>
-                        Личный кабинет
-                    </Button>
-                </MenuItem>
+            <Menu name="mainheader" borderless attached="top" inverted>
+                <Container style={{ width: "1330px", margin: "auto" }}>
+                    <MenuItem position="left" name="logo">
+                        <Link href="/">
+                            <Image src="/logo.svg" size="tiny" />
+                        </Link>
+                    </MenuItem>
+                    <MenuItem position="right">+77711739922</MenuItem>
+                    <MenuItem>
+                        <Button basic inverted>
+                            EN
+                        </Button>
+                        <Button basic inverted style={{ marginLeft: "10px" }}>
+                            USD
+                        </Button>
+                        <Button basic inverted style={{ marginLeft: "20px" }}>
+                            Личный кабинет
+                        </Button>
+                    </MenuItem>
+                </Container>
             </Menu>
             <Menu name="navbar" attached="bottom" secondary>
-                <MenuItem name="professions">Профессии</MenuItem>
+                <Container style={{ width: "1330px", margin: "auto" }}>
+                    <MenuItem name="professions">Профессии</MenuItem>
 
-                <MenuItem name="curators">Кураторы</MenuItem>
+                    <MenuItem name="curators">Кураторы</MenuItem>
 
-                <MenuItem name="tostudents">Студентам</MenuItem>
+                    <MenuItem name="tostudents">Студентам</MenuItem>
 
-                <Dropdown text="dropdown" pointing className="link item">
-                    <DropdownMenu>
-                        <DropdownItem>Вкладка 1</DropdownItem>
-                        <DropdownItem>Вкладка 2</DropdownItem>
-                        <DropdownItem>Вкладка 3</DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
+                    <Dropdown text="dropdown" pointing className="link item">
+                        <DropdownMenu>
+                            <DropdownItem>Вкладка 1</DropdownItem>
+                            <DropdownItem>Вкладка 2</DropdownItem>
+                            <DropdownItem>Вкладка 3</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
 
-                <MenuItem position="right">
-                    <Button
-                        style={{ padding: "8px", margin: "-12px" }}
-                        color="black"
-                        circular
-                        icon="telegram plane"
-                    />
-                </MenuItem>
-                <MenuItem>
-                    <Button
-                        style={{ padding: "8px", margin: "-12px" }}
-                        color="black"
-                        circular
-                        icon="vk"
-                    />
-                </MenuItem>
-                <MenuItem>
-                    <Button
-                        style={{ padding: "8px", margin: "-12px" }}
-                        color="black"
-                        circular
-                        icon="youtube"
-                    />
-                </MenuItem>
-                <MenuItem>
-                    <Button
-                        color="black"
-                        circular
-                        icon
-                        labelPosition="left"
-                        style={{ marginLeft: "20px" }}
-                    >
-                        <Icon name="cart" />
-                        Корзина
-                    </Button>
-                </MenuItem>
+                    <MenuItem position="right">
+                        <Button
+                            style={{ padding: "8px", margin: "-12px" }}
+                            color="black"
+                            circular
+                            icon="telegram plane"
+                        />
+                    </MenuItem>
+                    <MenuItem>
+                        <Button
+                            style={{ padding: "8px", margin: "-12px" }}
+                            color="black"
+                            circular
+                            icon="vk"
+                        />
+                    </MenuItem>
+                    <MenuItem>
+                        <Button
+                            style={{ padding: "8px", margin: "-12px" }}
+                            color="black"
+                            circular
+                            icon="youtube"
+                        />
+                    </MenuItem>
+                    <MenuItem>
+                        <Button
+                            color="black"
+                            circular
+                            icon
+                            labelPosition="left"
+                            style={{ marginLeft: "20px" }}
+                        >
+                            <Icon name="cart" />
+                            Корзина
+                        </Button>
+                    </MenuItem>
+                </Container>
             </Menu>
         </Container>
     );
