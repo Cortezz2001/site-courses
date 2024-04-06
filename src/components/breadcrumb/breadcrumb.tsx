236adbb651f47d14b8bb89f9f9fcfff46c80ae76
Maxim Fedkevich <cortezzcloud@gmail.com>
2024-04-06 13:19:10 +0500

    breadcrumb key fix

--- src/components/breadcrumb/breadcrumb.tsx
+++ src/components/breadcrumb/breadcrumb.tsx
@@ -1,6 +1,11 @@
 "use client";
 import React from "react";
-import { Breadcrumb, BreadcrumbDivider, BreadcrumbSection } from "@/UI/SUI";
+import {
+    Breadcrumb,
+    BreadcrumbDivider,
+    BreadcrumbSection,
+    Container,
+} from "@/UI/SUI";
 import { IBreadCrumb } from "./type";
 
 interface BreadcrumbProps {
@@ -11,25 +16,30 @@ export const BreadcrumbComponent: React.FC<BreadcrumbProps> = ({
     sections,
 }) => {
     return (
-        <div style={{ display: "flex", flexDirection: "row" }}>
-            {sections.map((section) => (
-                <Breadcrumb>
-                    <BreadcrumbSection
-                        link={section.isActive ? false : section.isLink}
-                        href={section.isActive ? undefined : section.url}
-                        style={{
-                            color: section.isActive ? "#1b1c1d" : "#007397",
-                        }}
-                        active={section.isActive}
-                    >
-                        {section.content}
-                    </BreadcrumbSection>
-                    {section.isActive ? null : (
-                        <BreadcrumbDivider icon="right angle" />
-                    )}
-                </Breadcrumb>
-            ))}
-        </div>
+        <Container style={{ display: "flex", flexDirection: "row" }}>
+            <Breadcrumb>
+                {sections.map((section, index) => (
+                    <React.Fragment key={index}>
+                        <BreadcrumbSection
+                            link={section.isActive ? false : section.isLink}
+                            href={section.isActive ? undefined : section.url}
+                            style={{
+                                color: section.isActive ? "#1b1c1d" : "#007397",
+                            }}
+                            active={section.isActive}
+                        >
+                            {section.content}
+                        </BreadcrumbSection>
+                        {section.isActive ? null : (
+                            <BreadcrumbDivider
+                                key={`divider-${index}`}
+                                icon="right angle"
+                            />
+                        )}
+                    </React.Fragment>
+                ))}
+            </Breadcrumb>
+        </Container>
     );
 };
 
