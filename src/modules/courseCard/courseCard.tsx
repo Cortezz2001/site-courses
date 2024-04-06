74f00ef12481dc16ae014d050636f245b7ef56f9
Maxim Fedkevich <cortezzcloud@gmail.com>
2024-04-07 02:11:16 +0500

    courses card grid fix

--- src/modules/courseCard/courseCard.tsx
+++ src/modules/courseCard/courseCard.tsx
@@ -52,15 +52,19 @@ export const CourseCard: React.FC<CardPropsCourse> = ({
             <CardContent
                 style={{
                     marginTop: "5px",
-                    display: "flex",
-                    flexDirection: "column",
+                    display: "grid",
+                    gridTemplateColumns: "1fr",
+                    gridTemplateRows: "1fr 40px 50px",
+                    gridGap: "6px",
                 }}
             >
                 <CardHeader>{header}</CardHeader>
                 <CardDescription
                     style={{ fontweight: "bold", marginBottom: "15px" }}
                 >
-                    <Header sub>Цена</Header>
+                    <Header sub style={{ margin: "0" }}>
+                        Цена
+                    </Header>
                     <span>{description}</span>
                 </CardDescription>
                 <CardMeta style={{ marginTop: "auto" }}>{extra}</CardMeta>
