a092b00ea96dad3d8197d080c3ee82f0728fc8fa
eimwe <prokopenkoserge@inbox.ru>
2024-04-08 15:14:45 +0500

    Updated page instances with event data props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import { Main } from "@/screens/main/page";
 import { CoachesService } from "@/service/coachesService/service";
 import { CoursesService } from "@/service/coursesService/service";
+import { EventsService } from "@/service/eventsService/service";
 import "semantic-ui-css/semantic.min.css";
 
 export default async function Home() {
     const coaches_ = await CoachesService.getCoaches();
     const courses_ = await CoursesService.getCourses();
+    const events_ = await EventsService.getEvents();
 
-    return <Main coachesInfo={coaches_} coursesInfo={courses_} />;
+    return (
+        <Main
+            coachesInfo={coaches_}
+            coursesInfo={courses_}
+            eventsInfo={events_}
+        />
+    );
 }
--- src/screens/main/page.tsx
+++ src/screens/main/page.tsx
@@ -1,17 +1,24 @@
 import { ICoachCardInfo } from "@/service/coachesService/types";
 import { ICourseCardInfo } from "@/service/coursesService/types";
+import { IEventCardInfo } from "@/service/eventsService/types";
 import { Layout } from "@/layouts/layout";
 import BannerCarousel from "./components/bannerCarousel";
 import { CoachesCarousel } from "./components/coachesCarousel";
 import { CoursesCarousel } from "./components/coursesCarousel";
+import { EventsCarousel } from "./components/eventsCarousel";
 import { Container, Header } from "@/UI/SUI";
 
 interface MainProps {
     coachesInfo: ICoachCardInfo[];
     coursesInfo: ICourseCardInfo[];
+    eventsInfo: IEventCardInfo[];
 }
 
-export const Main: React.FC<MainProps> = ({ coachesInfo, coursesInfo }) => {
+export const Main: React.FC<MainProps> = ({
+    coachesInfo,
+    coursesInfo,
+    eventsInfo,
+}) => {
     return (
         <Layout>
             <Container>
@@ -20,6 +27,8 @@ export const Main: React.FC<MainProps> = ({ coachesInfo, coursesInfo }) => {
                 <CoursesCarousel coursesInfo={coursesInfo} />
                 <Header as="h2">Наши тренеры</Header>
                 <CoachesCarousel coachesInfo={coachesInfo} />
+                <Header as="h2">Анонсы наших мероприятий</Header>
+                <EventsCarousel eventsInfo={eventsInfo} />
             </Container>
         </Layout>
     );
