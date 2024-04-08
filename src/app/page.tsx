fbab57f9f1e750159d7dce8f74fabfc3d6a5ad0f
eimwe <prokopenkoserge@inbox.ru>
2024-04-08 15:07:37 +0500

    Updated page instances with course data props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Main } from "@/screens/main/page";
 import { CoachesService } from "@/service/coachesService/service";
+import { CoursesService } from "@/service/coursesService/service";
 import "semantic-ui-css/semantic.min.css";
 
 export default async function Home() {
     const coaches_ = await CoachesService.getCoaches();
+    const courses_ = await CoursesService.getCourses();
 
-    return <Main coachesInfo={coaches_} />;
+    return <Main coachesInfo={coaches_} coursesInfo={courses_} />;
 }
--- src/screens/main/page.tsx
+++ src/screens/main/page.tsx
@@ -1,15 +1,23 @@
-import { ICoachCardInfoGroup } from "@/service/coachesService/types";
+import { ICoachCardInfo } from "@/service/coachesService/types";
+import { ICourseCardInfo } from "@/service/coursesService/types";
 import { Layout } from "@/layouts/layout";
 import BannerCarousel from "./components/bannerCarousel";
 import { CoachesCarousel } from "./components/coachesCarousel";
+import { CoursesCarousel } from "./components/coursesCarousel";
 import { Container, Header } from "@/UI/SUI";
 
-export const Main: React.FC<ICoachCardInfoGroup> = ({ coachesInfo }) => {
+interface MainProps {
+    coachesInfo: ICoachCardInfo[];
+    coursesInfo: ICourseCardInfo[];
+}
+
+export const Main: React.FC<MainProps> = ({ coachesInfo, coursesInfo }) => {
     return (
         <Layout>
             <Container>
                 <BannerCarousel />
                 <Header as="h2">Курсы</Header>
+                <CoursesCarousel coursesInfo={coursesInfo} />
                 <Header as="h2">Наши тренеры</Header>
                 <CoachesCarousel coachesInfo={coachesInfo} />
             </Container>
