2dfb27ea3685c2847ef3a8de0644709642870283
eimwe <prokopenkoserge@inbox.ru>
2024-04-09 17:34:53 +0500

    Fixed coach data fetching

--- src/service/coachDetailPageService/service.tsx
+++ src/service/coachDetailPageService/service.tsx
@@ -3,7 +3,7 @@ import { ICoachDetailPageInfo } from "./types";
 
 export const CoachDetailPageService = {
     async getCoaches(): Promise<Array<ICoachDetailPageInfo>> {
-        const res = await fetch(API_URL + URLS.getCourses);
+        const res = await fetch(API_URL + URLS.getCoaches);
         if (res.status !== 200) {
             throw new Error("Failed to fetch data");
         }
