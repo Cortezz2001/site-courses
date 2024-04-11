ba58b4fe9daf57bd5d8f4db22214e28908f6d901
Maxim Fedkevich <cortezzcloud@gmail.com>
2024-04-12 00:23:20 +0500

    coursecard skeleton width fix

--- src/modules/courseCard/courseCardSkeleton.tsx
+++ src/modules/courseCard/courseCardSkeleton.tsx
@@ -6,6 +6,7 @@ const CourseCardSkeleton = () => {
             className={style.shimmer}
             style={{
                 height: "100%",
+                width: "auto",
             }}
         >
             <Container
