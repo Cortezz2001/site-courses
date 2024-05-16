0a6daabd836d5f1f15d77dbef5a50d4dd3219202
Максим Федкевич <cortezz2001@gmail.com>
2024-05-16 21:09:44 +0500

    coach service types

--- src/service/coachesService/service.tsx
+++ src/service/coachesService/service.tsx
@@ -2,7 +2,7 @@ import { API_URL, LANGUAGES, URLS } from "../consts";
 import { ICoachCardInfo } from "./types";
 
 export const CoachesService = {
-    async getCoaches(language: LANGUAGES) {
+    async getCoaches(language: LANGUAGES): Promise<Array<ICoachCardInfo>> {
         const response = await fetch(API_URL + URLS.getCoaches, {
             headers: {
                 'Accept-Language': language
