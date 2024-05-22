7dac57f705dd5ead2d5f09153ead1f2ac6880464
Максим Федкевич <cortezz2001@gmail.com>
2024-05-22 23:52:15 +0500

    loading breadcrimbs deleted

--- src/app/[locale]/courses/loading.tsx
+++ src/app/[locale]/courses/loading.tsx
@@ -1,36 +1,12 @@
 import { Container, Loader } from "@/UI/SUI";
-import { BreadcrumbComponent } from "@/components/breadcrumb/breadcrumb";
-import { IBreadCrumb } from "@/components/breadcrumb/type";
 import { Layout } from "@/layouts/layout";
-import { useTranslations } from "next-intl";
-import { unstable_setRequestLocale } from "next-intl/server";
-type Props = {
-    params: {locale: string};
-  };
-export default function Loading({params: {locale}}: Props) {
-    unstable_setRequestLocale(locale);
-    const t = useTranslations("Breadcrumb");
-    const BreadcrumbProps: Array<IBreadCrumb> = [
-        {
-            key: "Home",
-            content: t('main'),
-            isLink: true,
-            isActive: false,
-            url: "/",
-        },
-        {
-            key: "Courses",
-            content: t('courses'),
-            isLink: true,
-            isActive: true,
-            url: "/courses",
-        },
-    ];
+
+export default function Loading() {
+
 
     return (
         <Layout>
             <>
-                <BreadcrumbComponent sections={BreadcrumbProps} />
                 <Container
                     style={{
                         minHeight: "50vh",
